17d36d24a153762f90eaa3be743882d67cc92858
Libra <myappname@163.com>
2021-03-18 19:16:53 +0800

    修改上传列表显示bug

--- packages/components/upload-table/src/index.vue
+++ packages/components/upload-table/src/index.vue
@@ -369,6 +369,10 @@ export default {
   background-color: #fff;
   border-radius: 5px;
   box-shadow: 0 2px 8px 0 rgba(0,0,0,0.14);
+  table {
+    display: table!important;
+  }
+
   &-buttons {
     padding: 16px;
     display: flex;
